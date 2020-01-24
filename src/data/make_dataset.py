a774b8a12b250da639f0a1ae0c97d2d211bcaadd
Joe J Collins <joejcollins@users.noreply.github.com>
2020-01-24 13:25:53 +0000

    Date formatting and test

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -2,21 +2,36 @@
 
 ...from the waypoints, survey sheets and photos """
 import csv
+import datetime
 
-FILES = ["/data/raw/2019-02/data-plant-2019-02-01-MEC.csv",
-         "/data/raw/2019-02/data-plant-2019-02-02-MEC.csv",
-         "/data/raw/2019-06/data-plant-2019-06-20-MEC.csv",
-         "/data/raw/2019-06/data-plant-2019-06-21-MEC.csv"]
+SURVEY_FILE_PATHS = ["./data/raw/2019-02/data-plant-2019-02-01-MEC.csv",
+                     "./data/raw/2019-02/data-plant-2019-02-02-MEC.csv",
+                     "./data/raw/2019-06/data-plant-2019-06-20-MEC.csv",
+                     "./data/raw/2019-06/data-plant-2019-06-21-MEC.csv"]
 
+def date_from_file(file_path):
+    """ Extract the survey date from the survey file path """
+    split_path = file_path.split("-")
+    year = int(split_path[-4])
+    month = int(split_path[-3])
+    day = int(split_path[-2])
+    return datetime.date(year, month, day)
 
 def main():
     """ Transform all the files """
-    with open("./data/processed/records.csv", 'w', newline='') as records_file:
-        record_writer = csv.writer(records_file, delimiter=' ',
-                                   quotechar='|', quoting=csv.QUOTE_MINIMAL)
-        record_writer.writerow("test") 
-    for survey_file in FILES:
-        pass
+    with open("./data/processed/records.csv", 'w+', newline='') as records_file:
+        record_writer = csv.DictWriter(records_file, fieldnames=["date", "quadrat", "waypoint",
+                                                                 "grid_reference", "photo_up",
+                                                                 "photo_down", "wetness",
+                                                                 "canopy", "species"])
+        record_writer.writeheader()
+
+        for survey_file_path in SURVEY_FILE_PATHS:
+            with open(survey_file_path, newline='') as survey_file:
+                survey_file_reader = csv.reader(survey_file, delimiter=',')
+                for row in survey_file_reader:
+                    formatted_date = date_from_file(survey_file_path).isoformat()
+                    record_writer.writerow({'date': formatted_date})
 
 
 if __name__ == "__main__":
