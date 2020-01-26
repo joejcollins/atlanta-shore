739202fcf43882ab842ac535908e30e81341a8a0
Joe J Collins <joejcollins@users.noreply.github.com>
2020-01-26 11:29:32 +0000

    Include the comments

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -10,6 +10,7 @@ SURVEY_FILE_PATHS = ["./data/raw/2019-02/data-plant-2019-02-01-MEC.csv",
                      "./data/raw/2019-06/data-plant-2019-06-20-MEC.csv",
                      "./data/raw/2019-06/data-plant-2019-06-21-MEC.csv"]
 
+
 def date_from_file(file_path):
     """ Extract the survey date from the survey file path """
     split_path = file_path.split("-")
@@ -20,39 +21,53 @@ def date_from_file(file_path):
 
 
 def main():
-    """ Transform all the survey files
-    
-    Create a set of records for each waypoint with the date and species identified.
+    """ Transform all the survey files into a records list
+
+    Create a records for each waypoint with the date and species identified.
     """
     with open("./data/processed/records.csv", 'w+', newline='') as records_file:
         record_writer = csv.DictWriter(records_file, fieldnames=["date", "quadrat", "waypoint",
                                                                  "grid_reference", "photo_up",
                                                                  "photo_down", "wetness",
-                                                                 "canopy", "species"])
+                                                                 "canopy", "species", "comments"])
         record_writer.writeheader()
 
         for survey_file_path in SURVEY_FILE_PATHS:
             with open(survey_file_path, newline='') as survey_file:
                 survey_file_reader = csv.reader(survey_file, delimiter=',')
-                # Add the date
-                record = {'date': date_from_file(survey_file_path).isoformat()}
+                # Prepare an empty record with the file date
+                record = {
+                    'date': date_from_file(survey_file_path).isoformat(),
+                    'comments': ''
+                }
+                waypoint_comments = ''  # to collect waypoint comments
                 for row in survey_file_reader:
-                    # Get the waypoint information.
+                    # Read the waypoint information into the record.
                     while 'species' not in row[0]:
-                        record[row[0]] = row[1]
+                        record[row[0]] = row[1]  # so just add it to the record
+                        if row[2]:  # there is a comment
+                            waypoint_comments = waypoint_comments + row[2]
                         row = next(survey_file_reader)
-                    # Get the individual species
+                    # Get the individual species records
                     while True:
+                        record['comments'] = waypoint_comments
                         record['species'] = row[1]
+                        if row[2]:  # there is a comment
+                            record['comments'] = record['comments'] + " - " + row[2]
                         # write a species record
                         record_writer.writerow(record)
                         try:
                             row = next(survey_file_reader)
                         except StopIteration:
-                            break # at the end of the file
+                            break  # at the end of the file
                         if re.match(r'species|^$', row[0]) == None:
-                            record[row[0]] = row[1] # add the read row to the record
-                            break # at the end of the species list
+                            # Next waypoint so add the read row to the record
+                            record[row[0]] = row[1]
+                            if row[2]:  # there is a comment
+                                waypoint_comments = row[2]
+                            else:
+                                waypoint_comments = ''
+                            break  # at the end of the species list
 
 
 if __name__ == "__main__":
