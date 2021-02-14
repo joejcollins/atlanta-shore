e2483e7efa66fb9495101888cedb86e43a4799fc
Joe J Collins <joejcollins@gmail.com>
2021-02-14 13:29:22 +0000

    R requirements

---
+++ requirements.r
@@ -0,0 +1,21 @@
+# Package list
+install.packages(c(
+    "base64enc", 
+    "digest", 
+    "evaluate" , 
+    "glue"  , 
+    "highr" , 
+    "htmltools", 
+    "jsonlite" , 
+    "knitr" , 
+    "magrittr" , 
+    "markdown" , 
+    "mime"  , 
+    "rlang" , 
+    "rmarkdown", 
+    "stringi"  , 
+    "stringr"  , 
+    "tinytex"  , 
+    "xfun"))
+
+tinytex::install_tinytex()
