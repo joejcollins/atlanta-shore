81da75a4a98e691f842c07ad3cf549011a4423be
Joe J Collins <joejcollins@users.noreply.github.com>
2021-06-21 18:30:17 +0100

    Alternative framework for parsing data

---
+++ src/data/site_record.py
@@ -0,0 +1,31 @@
+import csv
+import io
+
+class SiteRecord(object):
+
+    def __init__(self, site_record_string) -> None:
+        """ Initialize the site record attributes. """
+        super().__init__()
+        self.quadrat = None
+        self.waypoint = None
+        self.grid_reference = None
+        self.photo_up = None
+        self.photo_down = None
+        self.wetness = None
+        self.canopy = None
+        self.species = []
+        string_as_file = io.StringIO(site_record_string)
+        self._initialize_attributes()
+
+    def _initialize_attributes(self, string_as_file):
+        """ Set the site record values """
+        for row in string_as_file:
+            first = row[0]
+            second = row[1]
+            third = row[3]
+            match first:
+                case 'quadrat':
+                    self.quadrat = { 'id': second, 'comment': third }
+                case 'waypoint':
+                    self.waypoint = { 'name': second, 'comment': third }
+
