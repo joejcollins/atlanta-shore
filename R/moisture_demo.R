db15888a8fcc9ca1a627e5eff5567078593d20be
Joe Collins <joejcollins@gmail.com>
2023-06-23 08:57:36 +0000

    fix the short cuts

---
+++ R/moisture_demo.R
@@ -0,0 +1,10 @@
+library(ggplot2)
+
+# Generate example data
+moisture <- expand.grid(x = 1:10, y = 1:10)
+moisture$moisture <- runif(100, min = 0, max = 1)
+
+# Plot the grid of moisture values
+ggplot(moisture, aes(x, y, fill = moisture)) +
+  geom_tile() +
+  scale_fill_gradient(low = "white", high = "blue")
