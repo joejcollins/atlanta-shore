07bc0b934dd0f9bbeda1650349a1e20c4ec5634f
Joe Collins <joejcollins@gmail.com>
2023-06-24 09:20:20 +0000

    Draft test of reader

--- python/handlers/survey_file_reader.py
+++ python/handlers/survey_file_reader.py
@@ -1,7 +1,7 @@
 """Iterate through a survey data file."""
 
 class SurveyFileReader:
-    """"""
+    """Iterate through a survey data file."""
     
     def __init__(self, file_path):
         """Initialize with the survey file path."""
@@ -13,6 +13,7 @@ class SurveyFileReader:
         return self
 
     def __next__(self):
+        """Return the next record in the file."""
         if self.current_record is None:
             self.current_record = self._read_record()
 
@@ -25,20 +26,24 @@ class SurveyFileReader:
         return record
 
     def _read_record(self):
+        """Read from the file to the end of the species list."""
         record_lines = []
-        species_found = False
+        species_found = False  # The species list is the last field in the record.
         for line in self.file:
-            field_name, field_value = line.strip().split(',', 1)
-
+            # The species list is the last field in the record.
+            field_name, _ = line.strip().split(',', 1)
             if field_name.strip() == 'species':
                 record_lines.append(line.rstrip())
                 species_found = True
             elif species_found:
                 if field_name.strip():
-                    self.current_record = f'{field_name},{field_value}'
+                    self.current_record = '\n'.join(record_lines)
                     break
                 else:
                     record_lines.append(line.rstrip())
+            # Append the other lines to the record.
+            else:
+                record_lines.append(line.rstrip())
 
         if record_lines:
             return '\n'.join(record_lines)
--- tests/pytest/survey_file_reader_test.py
+++ tests/pytest/survey_file_reader_test.py
@@ -1,1 +1,24 @@
 """Confirm that the survey file reader can iterate through a file."""
+from python.handlers.survey_file_reader import SurveyFileReader
+
+
+def test_read_first_record() -> None:
+    """Confirm that the first record can be read."""
+    # Arrange
+    survey_file_reader = SurveyFileReader("tests/pytest/dummy_survey_file.csv")
+    # Act
+    record = next(survey_file_reader)
+    # Assert
+    assert record.startswith("quadrat,63,\nwaypoint,1,\ngrid_reference,TL6769933268,")
+
+
+def test_read_last_record() -> None:
+    """Confirm that the last record can be read."""
+    # Arrange
+    survey_file_reader = SurveyFileReader("tests/pytest/dummy_survey_file.csv")
+    # Act
+    record = None
+    for record in survey_file_reader:
+        pass
+    # Assert
+    assert record.endswith(",,,")
