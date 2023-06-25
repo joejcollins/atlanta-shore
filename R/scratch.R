7e7bc79cd67f644095cd22c0d8b647c9a4788bdb
Joe Collins <joejcollins@gmail.com>
2023-06-25 10:38:22 +0000

    Wetness test

--- R/scratch.R
+++ R/scratch.R
@@ -8,5 +8,18 @@ northings_origin <- min(sample_points$northings)
 sample_points$x <- (sample_points$eastings - eastings_origin) / 30
 sample_points$y <- (sample_points$northings - northings_origin) / 30
 
+observations <- read.csv("data/processed/observations.csv")
+observations$observation_date <- as.Date(observations$observation_date)
 
-                    
+merged_data <- merge(observations, sample_points, by = "sample_point_id")
+
+library(dplyr)
+start_date <- as.Date("2021-01-01")
+end_date <- as.Date("2021-12-31")
+
+observations_year <- merged_data %>% filter(observation_date >= start_date & observation_date <= end_date)
+
+library(ggplot2)
+ggplot(observations_year, aes(x, y, fill = wetness_estimate)) +
+  geom_tile() +
+  scale_fill_gradient(low = "white", high = "blue")
