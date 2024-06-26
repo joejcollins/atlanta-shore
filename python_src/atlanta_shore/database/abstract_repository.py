2f695880278e8057c4a25d218b38330866b58cc4
Joe Collins <joejcollins@gmail.com>
2024-06-26 11:30:06 +0000

    Draft abstract repository

---
+++ python_src/atlanta_shore/database/abstract_repository.py
@@ -0,0 +1,10 @@
+"""Abstract Repository."""
+import abc
+
+
+class AbstractRepository(abc.ABC):
+    """ """
+    @abc.abstractmethod
+    def create(self, record: dict) -> None:
+        """Create a record from a dict."""
+        raise NotImplementedError
