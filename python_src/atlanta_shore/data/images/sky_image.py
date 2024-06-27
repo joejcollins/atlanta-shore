11980948b0f72bcd91df9251bf3f40e19ded532d
Joe Collins <joejcollins@gmail.com>
2024-06-27 07:24:22 +0000

    Renamed

--- python_src/atlanta_shore/data/images/sky_image.py
+++ python_src/atlanta_shore/data/images/sky_image.py
@@ -1,15 +1,14 @@
 """fa"""
 
+
 class SkyImage(AbstractImageProcessor):
     def process_image(self):
         sky_amount = self.calculate_sky_amount()
         metadata = self.exif_data.get_metadata()
-        metadata['sky_amount'] = sky_amount
+        metadata["sky_amount"] = sky_amount
         self.save_metadata_to_db(metadata)
 
     def calculate_sky_amount(self):
         # Implement sky amount calculation logic
         sky_amount = 0.75  # Placeholder value
         return sky_amount
-
-    def 
