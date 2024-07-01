5f8d52d22abc2fa08eef0dae4a632ab607b43a4f
Joe Collins <joejcollins@gmail.com>
2024-07-01 15:55:12 +0000

    Not needed

--- src/data/survey_records.py
+++
@@ -1,9 +0,0 @@
-""" Representation of a set of site records. """
-
-
-class SurveyRecords(object):
-    def __init__(self, survey_file_path) -> None:
-        """ """
-        super().__init__()
-        self._survey_file_path = survey_file_path
-        # with open(survey_file_path, newline='') as survey_file:
